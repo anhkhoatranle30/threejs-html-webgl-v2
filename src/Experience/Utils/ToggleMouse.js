4a61488e7081c45dfc638b77b6d3624502f02123
khoatrn-dev <khoatrn.dev@gmail.com>
2021-11-29 15:46:34 +0700

    Cleaned up code in ToggleMouse class

--- src/Experience/Utils/ToggleMouse.js
+++ src/Experience/Utils/ToggleMouse.js
@@ -1,5 +1,7 @@
 import EventEmitter from './EventEmitter';
 
+const MOUSE_EVENTS = ['mousedown', 'mouseup', 'mousemove'];
+
 export default class ToggleMouse extends EventEmitter {
   constructor() {
     super();
@@ -8,19 +10,19 @@ export default class ToggleMouse extends EventEmitter {
       x: 0,
       y: 0,
     };
-    // Mousedown event
-    window.addEventListener('mousedown', (_event) => {
-      this.cursor.x = -(_event.clientX / window.innerWidth - 0.5) * 2;
-      this.cursor.y = (_event.clientY / window.innerHeight - 0.5) * 2;
 
-      this.trigger('mousedown');
-    });
-    // Mouseup event
-    window.addEventListener('mouseup', (_event) => {
-      this.cursor.x = -(_event.clientX / window.innerWidth - 0.5) * 2;
-      this.cursor.y = (_event.clientY / window.innerHeight - 0.5) * 2;
+    // Setup events
+    MOUSE_EVENTS.forEach((mouseEvent) => {
+      window.addEventListener(mouseEvent, (_event) => {
+        this.updateCursorByEvent(_event);
 
-      this.trigger('mouseup');
+        this.trigger(mouseEvent);
+      });
     });
+  }
+
+  updateCursorByEvent(_event) {
+    this.cursor.x = -(_event.clientX / window.innerWidth - 0.5) * 2;
+    this.cursor.y = (_event.clientY / window.innerHeight - 0.5) * 2;
   }
 }
