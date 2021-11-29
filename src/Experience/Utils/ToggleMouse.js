c00cdb1e83cdf0e9fefb2925d2ba2028fdcfc171
khoatrn-dev <khoatrn.dev@gmail.com>
2021-11-29 13:41:32 +0700

    Added toggleMouse class to take responsibility of toggling mouse

---
+++ src/Experience/Utils/ToggleMouse.js
@@ -0,0 +1,26 @@
+import EventEmitter from './EventEmitter';
+
+export default class Parallax extends EventEmitter {
+  constructor() {
+    super();
+    // Setup
+    this.cursor = {
+      x: 0,
+      y: 0,
+    };
+    // Mousedown event
+    window.addEventListener('mousedown', (_event) => {
+      this.cursor.x = -(_event.clientX / window.innerWidth - 0.5) * 2;
+      this.cursor.y = (_event.clientY / window.innerWidth - 0.5) * 2;
+
+      this.trigger('mousedown');
+    });
+    // Mouseup event
+    window.addEventListener('mouseup', (_event) => {
+      this.cursor.x = -(_event.clientX / window.innerWidth - 0.5) * 2;
+      this.cursor.y = (_event.clientY / window.innerWidth - 0.5) * 2;
+
+      this.trigger('mouseup');
+    });
+  }
+}
