5f4b07e043f1a7636acbe510773e247e26ae6e38
khoatrn-dev <khoatrn.dev@gmail.com>
2021-11-30 15:55:17 +0700

    Show environment on the scene

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -49,6 +49,8 @@ export default class Environment {
     this.environmentMap.texture.encoding = THREE.sRGBEncoding;
 
     this.scene.environment = this.environmentMap.texture;
+    this.scene.environment = this.environmentMap.texture;
+    this.scene.background = this.environmentMap.texture;
 
     this.environmentMap.updateMaterials();
   }
